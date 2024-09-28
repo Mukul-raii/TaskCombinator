a340eb652360e3c8364f3d11e5dd5ec1498c7d35
Mukul-raii <Mukurai2004@gmail.com>
2024-09-28 17:08:05 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors(
     {
-        origin: ["http://localhost:5173"],
+        origin: ["https://taskcombinator.vercel.app"],
         credentials: true
     }
 ))
